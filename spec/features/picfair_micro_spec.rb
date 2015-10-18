dd40426e2b125a3aa83dcee1ad1a9fc6b784b046
faheem <faheem@faheempatel.com>
2015-10-18 03:00:30 +0100

    Add view pic test

--- spec/features/picfair_micro_spec.rb
+++ spec/features/picfair_micro_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe "Picfair micro:", :type => :feature do
 
   feature 'As a user, I', js: true, focus: true do
     scenario 'can add a new pic' do
-      visit(new_pic_path)
-      fill_in('Name', :with => 'Some Image')
-      fill_in('Description', :with => 'blah blah')
-      fill_in('Url', :with => 'http://placekitten.com/300/350')
-      click_button('Create Pic')
+      # add a pic
+      add_image('http://placekitten.com/100/100')
+      # check for success message
       expect(page).to have_content 'Pic was successfully created.'
     end
 
     scenario 'view the pic I added' do
-      skip("TODO")
+      add_image('http://www.fillmurray.com/100/100', 'Bill Murray')
+      visit(pics_path)
+      find('img').click
+      expect(page).to have_content 'Name: Bill Murray'
     end
 
     scenario 'edit a pic' do
@@ -33,10 +34,10 @@ RSpec.describe "Picfair micro:", :type => :feature do
   end
 end
 
-def add_image(url)
-    visit(new_pic_path)
-    fill_in('Name', :with => 'Some Image')
-    fill_in('Description', :with => 'blah blah')
-    fill_in('Url', :with => url)
-    click_button('Create Pic')
+def add_image(url, title = 'Some Image')
+  visit(new_pic_path)
+  fill_in('Name', :with => title)
+  fill_in('Description', :with => 'blah blah')
+  fill_in('Url', :with => url)
+  click_button('Create Pic')
 end
