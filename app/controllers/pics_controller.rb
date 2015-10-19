4a9234fda5c44a9cddd180bbb5084ef552b24d82
faheem <faheem@faheempatel.com>
2015-10-19 07:41:43 +0100

    Add ability to star

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -69,6 +69,6 @@ class PicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pic_params
-      params.require(:pic).permit(:name, :description, :url, :created_at, :updated_at)
+      params.require(:pic).permit(:name, :description, :url, :created_at, :updated_at, :starred)
     end
 end
